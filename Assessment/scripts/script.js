259170bdd80d42eb0f90b11f8588bbef0dafcd32
m.d.elliott <m.d.elliott@outlook.com>
2019-11-14 17:37:22 -0600

    did some bookkeeping to make the code nicer and more modularized if I want to use it's many object extending functions in the future.

--- Assessment/scripts/script.js
+++ Assessment/scripts/script.js
@@ -1,12 +1,30 @@
-// initializes app by creating base todo.
-function init() {
-  addTodo(createNewTodo("Set up some todos!", randomIntFromInterval(0, 7)));
-  // addTodo(createNewTodo("Set up some todos!", randomIntFromInterval(0, 7)));
-  // addTodo(createNewTodo("Set up some todos!", randomIntFromInterval(0, 7)));
+// **********global variables***********
+const exampleTodos = [
+  "Set up some todos.",
+  "You can do this.",
+  "The clock is ticking!",
+  "Welcome to Bootstrap.",
+  "Close me!",
+  "Thank you for using our Todo service.",
+  "Hello, Dev10!",
+  "Shout-out to Gen10!",
+]
+
+// ****app functions****
+
+// initializes app by creating base todos.
+function init(count) {
+  if(!count) {
+    count = 1;
+  }
+  while(count) {
+    createExampleTodo();
+    count--
+  }
   resetTodoForm();
 }
 
-// ****app functions****
+// creates new todo element from task and day data.
 function createNewTodo(task, days) {
   var template = document.getElementById("todo-template");
   var todo = template.cloneNode(true);
@@ -14,18 +32,20 @@ function createNewTodo(task, days) {
   const urgency = getTodoUrgency(days);
   todo.className = "todo " + urgency;
   todo.querySelector('.todo-task').innerHTML = task;
-  todo.querySelector('.todo-days').innerHTML = pluralize(days, "day");
+  todo.querySelector('.todo-days').innerHTML = "day".pluralize(days);
   todo.setAttribute("days", parseInt(days));
   return todo;
 }
 
+// adds a todo element to the todo list.
 function addTodo(todo) {
-  const todo_list = document.getElementById("todo-list");
-  todo_list.appendChild(todo);
-  DOYPSort(todo_list, ".todo", "days", "L");
+  const todoList = document.getElementById("todo-list");
+  todoList.appendChild(todo.cloneNode(true));
+  todoList.DOYPSort(".todo", "days", "L");
 }
 
 // -------Form events----------
+//removes errors from the passed in form.
 function clearErrors(form) {
   var formElements = form.elements;
   var formElementsLength = formElements.length;
@@ -34,41 +54,41 @@ function clearErrors(form) {
     loopCounter++) {
     var element = formElements[loopCounter];
     if (!element.isValid) {
-            element.isValid = true;
+      element.isValid = true;
     }
   }
 }
 
+// resets the todo form specifically.
 function resetTodoForm() {
   const form = getTodoForm();
   clearErrors(form);
-  const task_field = form["task"];
-  const date_field = form["date"];
-  let days_field = form["days"];
-  resetPositiveIntegerField(days_field);
-  date_field.value = dateToString(today());
-  task_field.focus();
+  const taskField = form["task"];
+  const dateField = form["date"];
+  let daysField = form["days"];
+  resetPositiveIntegerField(daysField);
+  resetDateField(dateField);
+  taskField.focus();
 }
 
-function resetPositiveIntegerField(field) {
-  field.value = field.value >= 0
-    ? field.value
-    : 1;
-}
-
+// sets the days field by the date string passed in.
 function setDaysFieldByDate(date) {
-  days_field = getTodoForm()["days"];
-  days_field.value = daysBetweenNowAnd(date)
+  daysField = getTodoForm()["days"];
+  daysField.value = date.daysBetweenThisAndNow();
 }
 
+//sets the date field by the days string passed in.
 function setDateFieldByDays(days) {
-  date_field = getTodoForm()["date"];
-  date_field.value = dateToString(addDays(today(), parseInt(days)))
+  dateField = getTodoForm()["date"];
+  // dateField.value = dateToString(addDays(today(), parseInt(days)))
+  dateField.value = today().addDays(parseInt(days)).toStringYYYYMMDD();
 }
 
+//validates form and creates todo from form fields.
 function validateTodo() {
   var form = getTodoForm();
   clearErrors(form);
+
   var taskField = form["task"];
   var daysField = form["days"];
   // var dateField = form["date"];
@@ -88,7 +108,20 @@ function validateTodo() {
   }
 }
 
-// *******validation helper functions *******
+// *******Value validation functions *******
+// sets date inputs to today's date.
+function resetDateField(field) {
+  field.value = today().toStringYYYYMMDD();
+}
+
+// sets an integer field to be at least 0.
+function resetPositiveIntegerField(field) {
+  field.value = field.value >= 0
+    ? field.value
+    : 0;
+}
+
+// sets an integer field to invalid if it is populated with any negative or non-numerical value.
 function validatePositiveIntegerField(field, nameOfField) {
   var value = parseInt(field.value);
   if (field.value === "" || isNaN(field.value)) {
@@ -104,11 +137,12 @@ function validatePositiveIntegerField(field, nameOfField) {
 }
 
 // ****Helper functions****
-
+// gets the todo form from the dom.
 function getTodoForm() {
   return document.forms["todo-form"];
 }
 
+// retrieves the urgency of a todo based on number of days.
 function getTodoUrgency(days) {
   return (days < 3)
           ? "todo-urgent"
@@ -118,81 +152,122 @@ function getTodoUrgency(days) {
           "todo-standard";
 }
 
+// creates an example todo randomly selected from the examples repository.
+function createExampleTodo() {
+  const exampleTodo =
+    createNewTodo(exampleTodos.randomEntry(), (7).randomPositive());
+  addTodo(exampleTodo);
+}
+
 // ****utility functions******
+// retrieves a datetime object containing today's date.
 function today() {
   return new Date();
 }
 
-function dateToString(date) {
-  var month = (date.getMonth() + 1);
-  var day = date.getDate();
+// ***Object extending functions***
+
+// gets a random integer between this and an optional minimum.
+Number.prototype.randomPositive = function(min) { // min and max included
+  if(!min) {
+    min = 0;
+  }
+  return Math.floor(Math.random() * (this - min + 1) + min);
+}
+
+
+// converts a datetime object into a string with YYYYMMDD format.
+Date.prototype.toStringYYYYMMDD = function() {
+  var month = (this.getMonth() + 1);
+  var day = this.getDate();
   if (month < 10)
       month = "0" + month;
   if (day < 10)
       day = "0" + day;
-  var today = date.getFullYear() + '-' + month + '-' + day;
+  var today = this.getFullYear() + '-' + month + '-' + day;
   return today;
 }
 
-function daysBetweenNowAnd(date) {
+// gets the number of days between now and the supplied datetime object.
+Date.prototype.daysBetweenThisAndNow = function() {
   const days_constant = 24*3600*1000;
-  let date_differential = (date - today());
+  let date_differential = (this - new Date());
   const ret = Math.floor(date_differential / days_constant) + 1;
   return ret;
 }
 
-function addDays(dateObj, numDays) {
-  dateObj.setDate(dateObj.getDate() + numDays);
-  return dateObj;
+
+//adds a number of days to a datetime object.
+Date.prototype.addDays = function(numDays) {
+  this.setDate(this.getDate() + numDays);
+  return this;
 }
 
-function randomIntFromInterval(min, max) { // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function capitalizeFirstLetter(string)
-{
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function pluralize(count, noun, suffix = 's') {
-  return `${count} ${noun}${count !== 1 ? suffix : ''}`;
-}
-
-function getClosestElement (elem, selector) {
-	for ( ; elem && elem !== document; elem = elem.parentNode ) {
-		if ( elem.matches( selector ) ) return elem;
-	}
-	return null;
+// returns boolean of whether or this string has a value.
+String.prototype.isEmpty = function() {
+  return (this.length === 0 || !this.trim());
 };
 
-function closeThisElementParent(elem, parentClass) {
-  getClosestElement(elem, parentClass).remove();
+// capitalizes first letter of a string
+String.prototype.capitalizeFirstLetter = function()
+{
+  return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-function DOYPSort(wrapper, elementtosort, AttrToSort, orderof) {
-    let elements = Array.from(wrapper.querySelectorAll(elementtosort));
-    let sortedChildren = elements.sort(function (a, b) {
-        if (orderof === 'H') {
-            return +parseInt(b.getAttribute(AttrToSort)) - +parseInt(a.getAttribute(AttrToSort));
-        }
-        if (orderof === 'L') {
-            return +parseInt(a.getAttribute(AttrToSort)) - +parseInt(b.getAttribute(AttrToSort));
-        }
-    });
-    wrapper.emptyElement();
-    wrapper.appendChildren(sortedChildren);
+// pluralizes a string based on supplied count.
+String.prototype.pluralize = function(count, suffix = 's') {
+  return `${count} ${this}${count !== 1 ? suffix : ''}`;
 }
 
+// gets a random entry from an array.
+Array.prototype.randomEntry = function() {
+  return this[Math.floor(Math.random()*this.length)];
+}
+
+// gets the closest parent of this node with option to include selector.
+Node.prototype.getClosestParent = function(selector) {
+  if (typeof selector !== 'string' || !(selector instanceof String))
+    selector = "";
+  if(selector.isEmpty()) {
+    return this.parentNode;
+  } else {
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+      if ( elem.matches( selector ) ) return elem;
+    }
+  }
+  return null;
+}
+
+// removes parent of this node, removing this node and all siblings as well.
+Node.prototype.removeParent = function(identifier) {
+  this.getClosestParent(identifier).remove();
+}
+
+//removes all child nodes from node.
 Node.prototype.emptyElement = function() {
   while (this.firstChild) {
     this.firstChild.remove();
   }
 }
 
+//appends an array of elements to a node.
 Node.prototype.appendChildren = function(arrayOfNodes) {
   var length = arrayOfNodes.length;
   for (var i = 0; i < length; i++) {
     this.appendChild(arrayOfNodes[i]);
   }
 };
+
+//sorts the object in a parent node by attribute.
+Node.prototype.DOYPSort = function(elementtosort, AttrToSort, orderof) {
+    let elements = Array.from(this.querySelectorAll(elementtosort));
+    let sortedChildren = elements.sort(function (a, b) {
+        if (orderof === 'H') {
+            return +parseInt(b.getAttribute(AttrToSort)) - +parseInt(a.getAttribute(AttrToSort));
+        } else {
+            return +parseInt(a.getAttribute(AttrToSort)) - +parseInt(b.getAttribute(AttrToSort));
+        }
+    });
+    this.emptyElement();
+    this.appendChildren(sortedChildren);
+}
